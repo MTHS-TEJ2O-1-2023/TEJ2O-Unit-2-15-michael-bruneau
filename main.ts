6118d01492bfbacffba22a9e17f29589d445b681
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-11-15 17:10:17 -0500

    no understand problem

--- main.ts
+++ main.ts
@@ -5,5 +5,28 @@
  * This program makes a sprite move in a circle
 */
 
+// variables
+let sprite: game.LedSprite = null
+let xAxis: number = 0
+let yAxis: number = 0
+
 // setup
 basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  // set x and y axis to 0
+  xAxis = 0
+  yAxis = 0
+  sprite = game.createSprite(xAxis, yAxis)
+  while if (xAxis <= 4)
+      basic.pause(500)
+      sprite.set(LedSpriteProperty.X, xAxis)
+      sprite.set(LedSpriteProperty.Y, yAxis)
+      xAxis = xAxis + 1
+    } else {
+      basic.pause(500)
+      sprite.set(LedSpriteProperty.X, xAxis)
+      sprite.set(LedSpriteProperty.Y, yAxis)
+      yAxis = yAxis + 1
+    }
+})
