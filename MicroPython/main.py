59dd94485f4fcbe2c2be726a2b5451ef2dbf8362
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-11-16 17:15:12 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,33 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Michael
+Created on: Nov 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+# variables
+x_axis = 0
+y_axis = 0
 
-display.scroll("Hello, World!")
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        while y_axis <= 4:
+            while x_axis <= 3:
+                sleep(500)
+                display.set_pixel(x_axis, y_axis, 9)
+                x_axis = x_axis + 1
+            sleep(500)
+            display.set_pixel(x_axis, y_axis, 9)
+            y_axis = y_axis + 1
+        while y_axis >= 0:
+            while x_axis >= 1:
+                sleep(500)
+                display.set_pixel(x_axis, y_axis, 9)
+                x_axis = x_axis - 1
+            sleep(500)
+            display.set_pixel(x_axis, y_axis, 9)
+            y_axis = y_axis + 1
